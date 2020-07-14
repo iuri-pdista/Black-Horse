32588ba6f24dad722f19c98bab694cd4e2bbc977
iuri-pdista <correaiuri86@gmail.com>
2020-07-14 18:23:11 -0300

    Improved encrption

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::string::String;
 use std::io;
 use std::io::prelude::*;
 use self::crypto::digest::Digest;
-use self::crypto::sha2::Sha256;
+use self::crypto::sha2::Sha512;
 
 fn main() -> io::Result<()>{
     let mut text_content= String::from("");
@@ -26,7 +26,7 @@ fn create_and_write(text: String) -> io::Result<()>{
 
 fn substitue(input: String) -> io::Result<()>{
     let mut file = File::create("test.txt")?;
-    let mut hasher = Sha256::new();
+    let mut hasher = Sha512::new();
     hasher.input_str(&input);
     let hex: String = hasher.result_str();
     let final_string =  hex.into_bytes();
