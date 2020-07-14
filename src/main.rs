e69ba1759c8595386bc208b4fd314b790c487c3e
iuri-pdista <correaiuri86@gmail.com>
2020-07-13 22:37:17 -0300

    Add the read and store feature

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::string::String;
+use std::io;
+use std::io::prelude::*;
+
+fn main() -> io::Result<()>{
+    let mut text_content = String::from("");
+    let mut file = File::open("./test.txt")?;
+    file.read_to_string(&mut text_content)?;
+    Ok(())
+}
