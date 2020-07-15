222e472c2c899f2307fdc06ef90eda6a8769b34e
iuri-pdista <correaiuri86@gmail.com>
2020-07-15 01:00:36 -0300

    Improved features

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use std::env;
 use std::fs::File;
 use std::string::String;
 use std::io;
 use std::io::prelude::*;
 
-
 mod functions;
 
 fn main() -> io::Result<()>{
+    let sys_args: Vec<String> = env::args().collect();
+    let file_path = sys_args[1].clone();
     let mut text_content= String::from("");
     let new_text_content = String::from("HACKEADO PELO IURI");
-    let mut file = File::open("./test.txt")?;
+    let mut file = File::open(file_path)?;
     file.read_to_string(&mut text_content)?;
     functions::create_and_write(text_content);
     functions::substitue(new_text_content);
