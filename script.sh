1ed0a1fc2642bee79d27d56c83821a317d1d1a0f
iuri-pdista <correaiuri86@gmail.com>
2020-07-15 01:00:57 -0300

    Improved script

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-./rust_shell
+VAR=$(pwd)
+VAR="${VAR}/*.txt" 
+echo ""
+for f in $VAR
+do
+  echo "Processing $f file..."
+  # count number of lines and output that for file $f
+  echo"$f"	
+  ./rust_shell "$f"
+done  
+
 
 #SHELL
 
