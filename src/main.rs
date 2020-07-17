24bd894ccc5b12d7384f1c9203f3d90292a1ed9a
iuri-pdista <correaiuri86@gmail.com>
2020-07-17 20:21:01 -0300

    Started the addition of the multiple files feature

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,12 @@ mod functions;
 fn main() -> io::Result<()>{
     let sys_args: Vec<String> = env::args().collect();
     let file_path = sys_args[1].clone();
+    let count = sys_args[2].clone();
     let mut text_content= String::from("");
     let mut file = File::open(file_path)?;
     file.read_to_string(&mut text_content)?;
     let new_text_content: String = text_content.clone();
-    functions::create_and_write(text_content);
+    functions::create_and_write(text_content, count);
     functions::substitute(new_text_content);
     Ok(())
 }
