3f8e3c055cfd704752dcb6dee445f7bdec74a86d
iuri-pdista <correaiuri86@gmail.com>
2020-07-18 18:16:31 -0300

    Add the file selection through system parameters :sparkles:

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import socket
+from os import system
+import sys
+from sys import platform
 
+filePath = sys.argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(),1234))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
 count = 0
-file = open("test.txt", "rb")
+file = open(filePath, "rb")
 content = file.read(50000)
 s.send(content)
 content = file.read(50000)
