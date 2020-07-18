219b3efc829f0d4e0310cc45f27003790f7795c3
iuri-pdista <correaiuri86@gmail.com>
2020-07-18 18:09:10 -0300

    Updated imports

---
+++ test_virus/client.py
@@ -0,0 +1,14 @@
+import socket
+from sys import platform
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(),1234))
+
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
+count = 0
+file = open("test.txt", "rb")
+content = file.read(50000)
+s.send(content)
+content = file.read(50000)
+count += 1    
