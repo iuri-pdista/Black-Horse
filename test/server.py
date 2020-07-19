c899a3907397b478fd2adab342c9a942f351da8b
iuri-pdista <correaiuri86@gmail.com>
2020-07-19 15:33:04 -0300

    Improved robustness :recycle:

--- test/server.py
+++ test/server.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(),6660))
 s.listen(5)
-i = 0 
+i = 0
 
 while True:
     clientsocket, address = s.accept()
@@ -13,11 +13,16 @@ while True:
     print(numberOfFiles.decode("utf-8"))
     numberOfFilesInt = int(numberOfFiles)
     while i < numberOfFilesInt:
-        l = clientsocket.recv(50000)
-        file = open('file_'+ str(i)+".txt",'wb')
-        file.write(l)
-        i = i + 1
-        file.close()
+        try:
+            print("Receiving file...")
+            l = clientsocket.recv(50000)
+            file = open('file_'+ str(i)+".txt",'wb')
+            file.write(l)
+            i = i + 1 
+            file.close()
+        except Exception as error:
+            error = "Could not receive file"
+            print(error)
     clientsocket.close()
 
 s.close()
