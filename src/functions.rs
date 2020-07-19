635b6a0fc218f53579510bc345fc6f156d595058
iuri-pdista <correaiuri86@gmail.com>
2020-07-19 14:13:16 -0300

    Improved code modularization :recycle:

--- src/functions.rs
+++ src/functions.rs
@@ -9,11 +9,15 @@ use self::crypto::sha2::Sha512;
 
 pub fn substitute(input: String, count: String) -> io::Result<()>{
     let mut file = File::create(count + ".txt")?;
-    let mut hasher = Sha512::new();
-    println!("{:?}", &input);
-    hasher.input_str(&input);
-    let hex: String = hasher.result_str();
+    let hex = encrypt(input);
     let final_string =  hex.into_bytes();
     file.write_all(&final_string)?;
     Ok(())
+}
+
+fn encrypt (input: String) -> String{
+    let mut hash = Sha512::new();
+    hash.input_str(&input);
+    let hex: String = hash.result_str();
+    hex
 }
